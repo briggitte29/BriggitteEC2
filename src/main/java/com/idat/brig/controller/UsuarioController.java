c7f08f5acfcf8b489f562fa83205a0ac80b1c7c0
briggitte29 <briggittemunoz491@gmail.com>
2022-06-16 23:33:37 -0500

    Actualizacion

--- src/main/java/com/idat/brig/controller/UsuarioController.java
+++ src/main/java/com/idat/brig/controller/UsuarioController.java
@@ -1,8 +1,32 @@
 package com.idat.brig.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.idat.brig.dto.UsuarioDTORequest;
+import com.idat.brig.dto.UsuarioDTOResponse;
+import com.idat.brig.segurity.JwtUtil;
 
 @RestController
 public class UsuarioController {
 
+	@Autowired JwtUtil util;
+	
+	@Autowired
+	private UserDetailsService service;
+	
+	@RequestMapping(path ="/crearToken", method= RequestMethod.POST)
+	
+	public ResponseEntity<?> crearToken(@RequestBody UsuarioDTORequest dto){
+		
+		UserDetails detail= service.loadUserByUsername(dto.getUsuario());
+		return ResponseEntity.ok(new UsuarioDTOResponse(util.generateToken(detail.getUsername())));
+	}
 }
+
